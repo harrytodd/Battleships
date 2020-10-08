74087c1870b8df73650528f509a23f1795df8caf
Harry Todd <harrytodd1997@gmail.com>
2020-10-08 16:47:54 +0100

    added use of buttons

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+//* Grid Generator
 const playerGrid = document.querySelector('.player-grid')
 const compGrid = document.querySelector('.comp-grid')
 const width = 10
 const playerCells = []
 const compCells = []
+//* Button Logic
+const lengthButtonsArr = Array.from(document.querySelectorAll('.length-butt'))
+const colorButtonsArr = Array.from(document.querySelectorAll('.color-butt'))
+const confirmPlaceButt = document.querySelector('.confirm')
+const lengthDiv = document.querySelector('.length')
+const colorDiv = document.querySelector('.color')
+const placeShipDiv = document.querySelector('.place-ship')
+const startGameDiv = document.querySelector('.start-game')
 
-//* GRID GENERATOR
+function init() {
+  playerGridGen()
+  compGridGen()
+  playerGrid.classList.add('show')
+  lengthDiv.classList.add('show')
+}
+init()
+
+//* ********** GRID GENERATOR **********
 function playerGridGen() {
   let currNum = 1
   for (let i = 0; i < width ** 2; i++) {
@@ -26,7 +43,6 @@ function playerGridGen() {
     div.innerHTML = (div.id).toString()
   }
 }
-playerGridGen()
 
 function compGridGen() {
   let currNum = 1
@@ -49,9 +65,58 @@ function compGridGen() {
     div.innerHTML = (div.id).toString()
   }
 }
-compGridGen()
+
+//* ********** CLASSES **********
+
+const playerShips = new class {
+  constructor(length, color, position) {
+    this.length = length,
+    this.color = color,
+    this.position = position
+  }
+}
 
 
+const compShips = new class {
+  constructor(length, color, position) {
+    this.length = length,
+    this.color = color,
+    this.position = position
+  }
+}
 
 
+//* ********** BUTTON LOGIC **********
+const shipAttributes = [[1], [2], [3], [4]]
+let currShipLength = 0
+
+lengthButtonsArr.forEach((el) => {
+  el.addEventListener('click', () => {
+    el.classList.add('selected')
+    lengthDiv.classList.remove('show')
+    colorDiv.classList.add('show')
+    currShipLength = Number(el.innerHTML)
+    console.log(currShipLength)
+  })
+})
+
+colorButtonsArr.forEach((el) => {
+  el.addEventListener('click', () => {
+    colorDiv.classList.remove('show')
+    placeShipDiv.classList.add('show')
+    shipAttributes[currShipLength - 1].push(el.id)
+    console.log(shipAttributes)
+  })
+})
+
+confirmPlaceButt.addEventListener('click', () => {
+  placeShipDiv.classList.remove('show')
+  let allTrue = 0
+  lengthButtonsArr.forEach((el) => el.classList.contains('selected') ? allTrue++ : true)
+  if (allTrue === 4) {
+    startGameDiv.classList.add('show')
+  } else {
+    lengthDiv.classList.add('show')
+  }
+})
 
